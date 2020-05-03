3d5d2fb039d2fef56bdba4c0dd307b43c9e85bce
mgunes <mistafagunes@gmail.com>
2020-05-03 23:31:14 +0300

    improved guess game

--- modules/number_prediction.py
+++ modules/number_prediction.py
@@ -1,33 +1,63 @@
-from random import randint
-from time import sleep
+# from random import randint
+# from time import sleep
+#
+# random_number = randint(1,100)
+# estimate_count = 7
+#
+# while True:
+#
+#     guess = int(input('tahmininiz: '))
+#
+#     estimate_count
+#
+#     if guess < random_number:
+#         print('sorgulaniyor')
+#         sleep(1) # 1 sn ye program durur
+#         print('daha buyuk bir sayi giriniz')
+#         estimate_count -= 1
+#     elif guess > random_number:
+#         print('sorgulaniyor')
+#         sleep(1)  # 1 sn ye program durur
+#         print('daha kucuk bir sayi giriniz')
+#     else:
+#         print('sorgulaniyor')
+#         sleep(1)  # 1 sn ye program durur
+#         print('Tebrikler')
+#         break
+#
+#     if estimate_count == 0:
+#         print('tahmin hakkiniz bitti')
+#         break
+#
+#
+#
 
-random_number = randint(1,100)
-estimate_count = 7
+from random import randint
+
+small_number = 1
+big_number = 100
+random_number = randint(small_number, big_number)
 
 while True:
 
     guess = int(input('tahmininiz: '))
 
-    estimate_count
-
     if guess < random_number:
-        print('sorgulaniyor')
-        sleep(1) # 1 sn ye program durur
-        print('daha buyuk bir sayi giriniz')
-        estimate_count -= 1
+        if small_number > guess:
+            print("hani {}'dan buyuk sayi gicektin".format(small_number))
+        else:
+            small_number = guess
+            print(str(guess) + '\'dan daha buyuk bir sayi giriniz')
     elif guess > random_number:
-        print('sorgulaniyor')
-        sleep(1)  # 1 sn ye program durur
-        print('daha kucuk bir sayi giriniz')
+        if big_number < guess:
+            print("hani {}'dan kucuk sayi gicektin".format(big_number))
+        else:
+            big_number = guess
+            print(str(guess) + '\'dan daha kucuk bir sayi giriniz')
     else:
-        print('sorgulaniyor')
-        sleep(1)  # 1 sn ye program durur
-        print('Tebrikler')
+        print('boomm  kaybettiniz')
         break
 
-    if estimate_count == 0:
-        print('tahmin hakkiniz bitti')
-        break
 
 
 
